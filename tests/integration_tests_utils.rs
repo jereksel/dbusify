fe1806d6579cde8dacf9fb0c0ff3413675da5df4
Andrzej Ressel <jereksel@gmail.com>
2020-08-02 14:20:46 +0200

    Create rust.yml (#2)

--- tests/integration_tests_utils.rs
+++ tests/integration_tests_utils.rs
@@ -6,7 +6,7 @@ use rspotify_hyper;
 use rspotify::spotify::client::Spotify;
 use dbus::ConnPath;
 use dbus::Connection;
-use std::panic;
+use std::{panic, env};
 use rspotify::spotify::oauth2::SpotifyOAuth;
 use rspotify::spotify::oauth2::SpotifyClientCredentials;
 use std::sync::Arc;
@@ -17,19 +17,25 @@ use std::time::Duration;
 use dbus::BusType;
 
 use dbusify::AccountType;
-use self::rspotify_hyper::get_token_hyper;
+use rspotify_hyper::get_token_hyper;
 
 pub fn run_test_type<T>(_type: AccountType, test: T) -> ()
     where T: FnOnce(Spotify, ConnPath<'_, &Connection>) -> () + panic::UnwindSafe
 {
 
-    let (client_id, client_secret) = dbusify::get_client();
+    let client_id = env::var("CLIENT_ID");
+    let client_secret = env::var("CLIENT_SECRET");
+
+    if client_id.is_err() || client_secret.is_err() {
+        println!("CLIENT_ID or CLIENT_SECRET not provided");
+        return;
+    }
 
     const REDIRECT_URL: &'static str = "http://localhost:8888/callback";
 
     let oauth = SpotifyOAuth::default()
-        .client_id(client_id.as_str())
-        .client_secret(client_secret.as_str())
+        .client_id(client_id.unwrap().as_str())
+        .client_secret(client_secret.unwrap().as_str())
         .redirect_uri(REDIRECT_URL)
         .cache_path(_type.get_config_file())
         .scope("playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify-private streaming ugc-image-upload user-follow-modify user-follow-read user-library-read user-library-modify user-read-private user-read-birthdate user-read-email user-top-read user-read-playback-state user-modify-playback-state user-read-currently-playing user-read-recently-played")
